3e14151bd9df58537830f8c3f7f7cd8aae8bbf57
Jurjen Munneke <jurjenmunneke@me.com>
2016-08-12 15:04:38 +0200

    Add better questions controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def one
+  end
+
+  def two
+  end
+
+  def three
+  end
+
+  def four
+  end
+
+  def five
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'pages/home'
-  get 'questions/1'
-  get 'questions/2'
-  get 'questions/3'
-  get 'questions/4'
-  get 'questions/5'
+  get 'questions/one'
+  get 'questions/two'
+  get 'questions/three'
+  get 'questions/four'
+  get 'questions/five'
   get 'rankings/score'
 
 end
