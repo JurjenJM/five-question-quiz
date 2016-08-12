be2607482c7dfee45cdf1ce4251ccfe09428d2f8
Jurjen Munneke <jurjenmunneke@me.com>
2016-08-12 14:49:16 +0200

    Change styling routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'rankings/score'
-
   root to: 'pages#home'
 
   get 'pages/home'
-
   get 'questions/1'
-
   get 'questions/2'
-
   get 'questions/3'
-
   get 'questions/4'
-
   get 'questions/5'
+  get 'rankings/score'
 
 end
