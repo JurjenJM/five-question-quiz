60e5b6599e6189e85fb45d48d4b52d9b7265ef92
Jurjen Munneke <jurjenmunneke@me.com>
2016-08-13 19:34:51 +0200

    Fix new and create game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,9 +7,14 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(params.require(:game).permit(:nickname, :q1_points, :q2_points, :q3_points, :q4_points, :q5_points))
+    @game[:score] = @game[:q1_points] + @game[:q2_points] + @game[:q3_points] + @game[:q4_points] + @game[:q5_points]
+    @game.save!
+    redirect_to games_path(@game)
   end
 
 end
