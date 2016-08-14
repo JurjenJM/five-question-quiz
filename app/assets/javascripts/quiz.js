ad20069e69f661a3ec9dc855b8e01421c899eeb2
Jurjen Munneke <jurjenmunneke@me.com>
2016-08-14 17:01:13 +0200

    Change time per question two 12 seconds

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -9,12 +9,12 @@ $('.submit').hide(); // Initially submit button wil be hidden.
 
  $('.start-quiz').click(function() {
   $('.welcome').hide();
-  $('.question-1').show().delay(4500).fadeOut(); // Question 1 shows on button click
-  $(setTimeout(function(){ $('.question-2').show().delay(4500).fadeOut(); }, 5000));
-  $(setTimeout(function(){ $('.question-3').show().delay(4500).fadeOut(); }, 10000));
-  $(setTimeout(function(){ $('.question-4').show().delay(4500).fadeOut(); }, 15000));
-  $(setTimeout(function(){ $('.question-5').show().delay(4500).fadeOut(); }, 20000));
-  $(setTimeout(function(){ $('.submit').show(); }, 25000));
+  $('.question-1').show().delay(11500).fadeOut(); // Question 1 shows on button click
+  $(setTimeout(function(){ $('.question-2').show().delay(11500).fadeOut(); }, 12000));
+  $(setTimeout(function(){ $('.question-3').show().delay(11500).fadeOut(); }, 24000));
+  $(setTimeout(function(){ $('.question-4').show().delay(11500).fadeOut(); }, 36000));
+  $(setTimeout(function(){ $('.question-5').show().delay(11500).fadeOut(); }, 48000));
+  $(setTimeout(function(){ $('.submit').show(); }, 60000));
 
   });
 
